47d456e4bd342bb6182a1bc2bc4af0c798e12633
Darya <dasha.karmaz@yandex.ru>
2019-09-27 18:37:44 +0300

    Реализована функция поиска НОД

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -21,3 +21,48 @@ def is_prime(n: int) -> bool:
         else:
             is_prime = True
     pass
+
+def gcd(a: int, b: int) -> int:
+    """
+    >>> gcd(12, 15)
+    3
+    >>> gcd(3, 7)
+    1
+    """
+    while a != b and a != 0 and b != 0:
+        if a > b:
+            a = a % b
+        elif b > a:
+            b = b % a
+    if a == 0:
+        gcd = b
+    else:
+        gcd = a
+    pass    
+
+def generate_keypair(p: int, q: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
+    if not (is_prime(p) and is_prime(q)):
+        raise ValueError('Both numbers must be prime.')
+    elif p == q:
+        raise ValueError('p and q cannot be equal')
+
+    # n = pq
+    n = p * q
+
+    # phi = (p-1)(q-1)
+    phi = (p-1) * (q-1)
+
+    # Choose an integer e such that e and phi(n) are coprime
+    e = random.randrange(1, phi)
+
+    # Use Euclid's Algorithm to verify that e and phi(n) are comprime
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+
+    # Use Extended Euclid's Algorithm to generate the private key
+    d = multiplicative_inverse(e, phi)
+    # Return public and private keypair
+    # Public key is (e, n) and private key is (d, n)
+    return ((e, n), (d, n))
