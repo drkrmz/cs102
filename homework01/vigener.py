dacf34404c07b37051280f5d60dd077f416755e5
Darya <dasha.karmaz@yandex.ru>
2019-09-27 17:52:11 +0300

    Реализована функция encrypt_vigenere

--- homework01/vigener.py
+++ homework01/vigener.py
@@ -7,9 +7,23 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    # PUT YOUR CODE HERE
+    ciphertext = ''
+    i = 0
+    while i < len(plaintext):
+        symbol = plaintext[i]
+        shift = keyword [i % len(keyword)]
+        if ord(shift) in range(65, 91):
+            if ord(symbol)+ord(shift)-65 < 91:
+                ciphertext = ciphertext + chr(ord(shift)+ord(symbol)-65)
+            else:
+                ciphertext = ciphertext + chr(ord(shift)+ord(symbol)-65-26)
+        else:
+            if ord(symbol)+ord(shift)-97 < 123:
+                ciphertext = ciphertext + chr(ord(shift)+ord(symbol)-97)
+            else:
+                ciphertext = ciphertext + chr(ord(shift)+ord(symbol)-97-26)
+        i = i + 1
     return ciphertext
-
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
