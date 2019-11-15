e1de23a82665595a81bb3b4ed288f5e520da79dc
Darya <dasha.karmaz@yandex.ru>
2019-11-16 00:12:39 +0300

    make it better

---
+++ k.py
@@ -0,0 +1,25 @@
+@bot.message_handler(commands=['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'])
+def get_schedule(message):
+    """ Получить расписание на указанный день """
+    day, week = message.text.split()
+    day = [:1]
+    soup = BeautifulSoup(web_page, "html.parser")
+    schedule_table = ''
+
+    # Получаем таблицу с расписанием на понедельник
+    schedule_table = soup.find("table", attrs={"id": day})
+
+    # Время проведения занятий
+    times_list = schedule_table.find_all("td", attrs={"class": "time"})
+    times_list = [time.span.text for time in times_list]
+
+    # Место проведения занятий
+    locations_list = schedule_table.find_all("td", attrs={"class": "room"})
+    locations_list = [room.span.text for room in locations_list]
+
+    # Название дисциплин и имена преподавателей
+    lessons_list = schedule_table.find_all("td", attrs={"class": "lesson"})
+    lessons_list = [lesson.text.split('\n\n') for lesson in lessons_list]
+    lessons_list = [', '.join([info for info in lesson_info if info]) for lesson_info in lessons_list]
+
+    return times_list, locations_list, lessons_list
