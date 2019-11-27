c87cb80d2ec78cde390a64b354644d200c8a7715
Darya <dasha.karmaz@yandex.ru>
2019-11-27 17:07:44 +0300

    Добавлен файл

---
+++ homework05/config.py
@@ -0,0 +1,11 @@
+VK_CONFIG = {
+    'domain': 'https://api.vk.com/method',
+    'access_token': 'd3b8ae6ca585bfab73b08a39e213114f8021a3e63446d0b8a6dc59462906371e0ac9ac0d4a22eab0a9a76',
+    'version': '5.87',
+}
+
+
+PLOTLY_CONFIG = {
+    'username': 'Имя пользователя Plot.ly',
+    'api_key': 'Ключ доступа Plot.ly',
+}
