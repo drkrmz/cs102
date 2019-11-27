4aa117d70c97f668141aaa8b448ae328d5cc2bb5
Darya <dasha.karmaz@yandex.ru>
2019-11-27 16:09:24 +0300

    Реализована функция age_predict()

---
+++ homework05/age.py
@@ -0,0 +1,34 @@
+import datetime as dt
+from statistics import median
+from typing import Optional
+
+from api import get_friends
+from api_models import User
+
+
+def age_predict(user_id: int) -> Optional[float]:
+    """ Наивный прогноз возраста по возрасту друзей
+    Возраст считается как медиана среди возраста всех друзей пользователя
+    :param user_id: идентификатор пользователя
+    :return: медианный возраст пользователя
+    """
+    assert isinstance(user_id, int), "user_id must be positive integer"
+    assert user_id > 0, "user_id must be positive integer"
+    friends = get_friends(user_id, 'bdate')
+    ages = []
+    for i in range(friends['response']['count']):
+        try:
+            bdate = friends['response']['items'][i]['bdate']
+        except KeyError:
+            pass
+        else:
+            blist = bdate.split('.')
+            if len(blist) == 3:
+                day = dt.datetime.now().day
+                month = dt.datetime.now().month
+                year = dt.datetime.now().year
+                if month > int(blist[1]) or month == int(blist[1]) and day >= int(blist[0]):
+                    ages.append(year - int(blist[2]))
+                else:
+                    ages.append(year - int(blist[2]) - 1)
+    return median(ages)
