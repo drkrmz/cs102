da6dd1d94be2d169b0680009d1c69409e187d705
Darya <dasha.karmaz@yandex.ru>
2019-12-07 09:25:23 +0300

    Новый токен

--- homework05/config.py
+++ homework05/config.py
@@ -1,6 +1,6 @@
 VK_CONFIG = {
     'domain': 'https://api.vk.com/method',
-    'access_token': 'ff85d7032af954e92455554ac545a1964128bd6a438016a32572e64b0256211433949f97634b452088f81',
+    'access_token': '5ac73047ed2b88509f16483f8c54f5b02d06db4c997a7c3b6d1e660da4d2ec8a1e102f85a5f02ab9ba37',
     'version': '5.87',
 }
 
