1b47aa465f3fc1310a5882eb08e315081b4ec019
Darya <dasha.karmaz@yandex.ru>
2020-02-21 13:11:08 +0300

    Код прошёл через mypy и pycodestyle

--- homework06/bayes.py
+++ homework06/bayes.py
@@ -2,16 +2,17 @@ from collections import Counter
 from math import log
 from numpy import argmax
 
+
 class NaiveBayesClassifier:
 
-    def __init__(self, alpha = 0.05):
+    def __init__(self, alpha=0.05):
         self.alpha = alpha
 
     def fit(self, X, y):
         self.labels_list = []
-        self.n_news = len(y) 
+        self.n_news = len(y)
         self.n_labels = len(dict(Counter(y)))
-        self.y_counter = Counter(y)     
+        self.y_counter = Counter(y)
         labeled_words = []
         for i in range(self.n_labels):
             self.labels_list.append(list(Counter(y))[i])
@@ -20,12 +21,12 @@ class NaiveBayesClassifier:
             for word in X[i].split():
                 j = 0
                 while not y[i] in self.labels_list[j]:
-                    j +=1
+                    j += 1
                 labeled_words[j].append(word)
         labels_count = []
-        n_words = [] 
-        all_words = [] 
-        self.P_words = [] 
+        n_words = []
+        all_words = []
+        self.P_words = []
         for i in range(self.n_labels):
             n_words.append(sum(Counter(labeled_words[i]).values()))
             labels_count.append(Counter(labeled_words[i]))
@@ -35,25 +36,24 @@ class NaiveBayesClassifier:
         for word in all_words:
             for i in range(self.n_labels):
                 value = (labels_count[i][word] + self.alpha) / (n_words[i] + (self.alpha * len(all_words)))
-                self.P_words[i].update({word : value})
+                self.P_words[i].update({word: value})
 
     def predict(self, X):
-        news_predict = [] 
-        P_each = [] 
-        ln = [] 
-        for i in range(self.n_labels): 
+        news_predict = []
+        P_each = []
+        ln = []
+        for i in range(self.n_labels):
             ln.append(0)
             P_each.append(self.y_counter[self.labels_list[i]]/self.n_news)
-        for i in range(len(X)):    
-            for j in range(self.n_labels): 
+        for i in range(len(X)):
+            for j in range(self.n_labels):
                 ln[j] = log(P_each[j])
-            for word in X[i].split(): 
-                for j in range(self.n_labels): 
-                    if word in self.P_words[j]: 
-                        ln[j] += log(self.P_words[j][word]) 
+            for word in X[i].split():
+                for j in range(self.n_labels):
+                    if word in self.P_words[j]:
+                        ln[j] += log(self.P_words[j][word])
             news_predict.append(self.labels_list[argmax(ln)])
         return news_predict
-
 
     def score(self, X_test, y_test):
         y = self.predict(X_test)
