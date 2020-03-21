314dda77bf3132cd206e4ec6aef11625d5df56bf
Darya <dasha.karmaz@yandex.ru>
2020-03-21 20:26:10 +0300

    Добавлена папка

---
+++ homework11/load_data_to_postgres.py
@@ -0,0 +1,38 @@
+import psycopg2
+import csv
+
+conn = psycopg2.connect("host=localhost port=5433 dbname=odscourse user=postgres password=secret")
+cursor = conn.cursor()
+
+query = """
+CREATE TABLE IF NOT EXISTS telecom_churn (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR,
+    sex VARCHAR,
+    age REAL,
+    height REAL,
+    weight REAL,
+    team VARCHAR,
+    noc VARCHAR,
+    games VARCHAR,
+    year INTEGER,
+    season VARCHAR,
+    city VARCHAR,
+    sport VARCHAR,
+    event VARCHAR,
+    medal VARCHAR,
+)
+"""
+cursor.execute(query)
+conn.commit()
+
+with open('homework11/athlete_events.csv', 'r') as f:
+    reader = csv.reader(f)
+    # Skip the header row
+    next(reader)
+    for Id, row in enumerate(reader):
+        cursor.execute(
+            "INSERT INTO athlete_events VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+            [Id] + row
+        )
+conn.commit()
